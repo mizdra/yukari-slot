e7d2eed221162933c713f1d114781c319dc387f8
mizdra <pp.mizdra@gmail.com>
2018-11-08 23:38:41 +0900

    Web Animations APIのpolyfillを追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css'
 import jaJP from 'antd/lib/locale-provider/ja_JP'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'web-animations-js'
 import { App } from './App'
 
 ReactDOM.render((
