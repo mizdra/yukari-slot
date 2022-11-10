fe6a0f7142ef03c6496a6e3fe36bfa9cb73c2430
mizdra <pp.mizdra@gmail.com>
2022-11-11 00:34:26 +0900

    React 18 の破壊的変更に対応する

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,14 @@ import { ConfigProvider } from 'antd';
 import 'antd/dist/antd.css';
 import jaJP from 'antd/lib/locale-provider/ja_JP';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'web-animations-js';
 import { App } from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('app')!);
+
+root.render(
   <ConfigProvider locale={jaJP}>
     <App />
   </ConfigProvider>,
-  document.getElementById('app'),
 );
